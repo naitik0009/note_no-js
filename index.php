8a44d5de633dcc4b77d301f8658d18667e694530
naitik0009 <64009612+naitik0009@users.noreply.github.com>
2020-08-12 17:12:49 -0400

    Update index.php

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ if(isset($_POST['update'])){
 }
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Notes</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
